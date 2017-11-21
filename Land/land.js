3cf6964d78056a8dac8505d616cd3e4a6ec76209
Emil Sørhaug Eriksen <esorhaugeriksen@gmail.com>
2017-11-21 08:18:42 +0100

    update

--- Land/land.js
+++ Land/land.js
@@ -10,15 +10,28 @@ function setup() {
     };
     firebase.initializeApp(config);
     let divListe = document.getElementById("liste");
-    let ref = database().ref("nations");
+    let ref = firebase.database().ref("nations");
 
     function visLand(snapshot) {
-        let land = snapshot.val();
+        let navn = snapshot.key;
+        let info = snapshot.val();
         divListe.innerHTML += `
-        <div>$(land.capital)</div>
+          <div>
+            <h4>${navn}</h4>
+            <ul>
+             <li>Capital ${info.capital}
+             <li>${info.title} ${info.leader}
+             <li> Perks
+                <ul>
+                    <li> Money: ${info.perk.money}
+                    <li> Move: ${info.perk.move}
+                    <li> War: ${info.perk.war}
+                    <li> Science: ${info.perk.science}
+                </ul>
+            </ul>
+          </div>
         `;
     }
-    
     ref.on("child_added", visLand);
 
 }
